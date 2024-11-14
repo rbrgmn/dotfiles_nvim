9482216ba409182a06df74c30dfe48c3ad936abb
rbrgmn <roman.bergman@tutanota.com>
2024-11-14 16:19:50 +0200

    Change bufferline settings
    Set separator_style to slant /     \

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,12 @@
+--    DATE: 2024-11-12
+--  AUTHOR: RBRGMN <roman.bergman@tutanota.com>
+-- LICENSE: AGPL3.0
+
+return {
+	"akinsho/bufferline.nvim",
+	opts = {
+		options = {
+			separator_style = "slant",
+		},
+	},
+}
