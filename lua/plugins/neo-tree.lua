94e5c8e21754349bd254accde21da279d1811918
rbrgmn <roman.bergman@tutanota.com>
2024-11-14 16:20:58 +0200

    Change neo-tree keybinding

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,10 @@
+--    DATE: 2024-11-12
+--  AUTHOR: RBRGMN <roman.bergman@tutanota.com>
+-- LICENSE: AGPL3.0
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{ "<leader>e", ":Neotree focus<CR>", silent = true, desc = "Neotree open or Neotree focus" },
+	},
+}
